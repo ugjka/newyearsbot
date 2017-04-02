8b09ef68545eb59c2dc2d8b50bd2d29fb8388a7b
ugjka <ugis.germanis@gmail.com>
2017-04-02 16:09:00 +0300

    off by one error in target

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func init() {
 var target = func() time.Time {
 	tmp := time.Now().UTC()
 	if tmp.Month() == time.January && tmp.Day() < 2 {
-		return time.Date(tmp.Year()-1, time.January, 1, 0, 0, 0, 0, time.UTC)
+		return time.Date(tmp.Year(), time.January, 1, 0, 0, 0, 0, time.UTC)
 	}
 	return time.Date(tmp.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
 }()
