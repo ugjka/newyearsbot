7d9a6d1ae93ad37a8d78bf8bacb0b4bff0c5aa6d
ugjka <ugis.germanis@gmail.com>
2017-04-02 16:26:20 +0300

    off by one error

--- validatetz/main.go
+++ validatetz/main.go
@@ -21,7 +21,7 @@ import (
 var target = func() time.Time {
 	tmp := time.Now().UTC()
 	if tmp.Month() == time.January && tmp.Day() < 2 {
-		return time.Date(tmp.Year()-1, time.January, 1, 0, 0, 0, 0, time.UTC)
+		return time.Date(tmp.Year(), time.January, 1, 0, 0, 0, 0, time.UTC)
 	}
 	return time.Date(tmp.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
 }()
