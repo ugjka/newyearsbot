655937a78a39400ca08b57d150cb5c023a6530ab
ugjka <ugis.germanis@gmail.com>
2018-02-14 21:56:47 +0200

    Revert "abstract time now for proper testing"
    This reverts commit 99af3cb85498da80e8e16be4917924868bb80efb.

--- nyb/target.go
+++ nyb/target.go
@@ -8,10 +8,6 @@ var target = func() time.Time {
 	if tmp.Month() == time.January && tmp.Day() < 2 {
 		return time.Date(tmp.Year(), time.January, 1, 0, 0, 0, 0, time.UTC)
 	}
-	return time.Date(tmp.Year(), time.February, 15, 0, 0, 0, 0, time.UTC)
-	//return time.Date(tmp.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
+	//return time.Date(tmp.Year(), time.February, 14, 0, 0, 0, 0, time.UTC)
+	return time.Date(tmp.Year()+1, time.January, 1, 0, 0, 0, 0, time.UTC)
 }()
-
-var timeNow = func() time.Time {
-	return time.Now()
-}
