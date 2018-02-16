b8773aa95f76b22e7b1b71ac98a28a98bc90b8be
ugjka <ugis.germanis@gmail.com>
2018-02-16 05:35:36 +0200

    improve test

--- nyb/utils_test.go
+++ nyb/utils_test.go
@@ -10,24 +10,24 @@ func TestChangeNick(t *testing.T) {
 	for i := 1; i <= 4; i++ {
 		ln = changeNick(ln)
 		if got := strings.Count(ln, "_"); got != i {
-			t.Errorf("expecting %d _'s got %d ", i, got)
+			t.Errorf("expecting %d _'s got %d, string: %s", i, got, ln)
 		}
 		if len(ln) != 12+i {
-			t.Errorf("expecting lenght %d got %d", 12+i, len(ln))
+			t.Errorf("expecting lenght %d got %d, string: %s", 12+i, len(ln), ln)
 		}
 	}
 	if ln := changeNick(ln); len(ln) != 12 {
-		t.Errorf("expecting lenght 12 got %d", len(ln))
+		t.Errorf("expecting lenght 12 got %d, string: %s", len(ln), ln)
 	}
 	n := "a"
 	for i := 1; i <= 15; i++ {
 		n = changeNick(n)
 		c := strings.Count(n, "_")
 		if c != i {
-			t.Errorf("expecting %d _'s got %d: string: %s", i, c, n)
+			t.Errorf("expecting %d _'s got %d, string: %s", i, c, n)
 		}
 	}
 	if n := changeNick(n); n != "a" {
-		t.Error("n wasn't 'a'")
+		t.Error("nick wasn't 'a'")
 	}
 }
