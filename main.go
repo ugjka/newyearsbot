174d5f9c78538b6b182862b39bff79d5b826bbe9
ugjka <esesmu@protonmail.com>
2020-10-24 19:35:25 +0300

    better cli flag names

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 )
 
 //Custom flag to get irc channelsn to join
-var chans nyb.IrcChans
+var channels nyb.IrcChans
 
 func init() {
-	flag.Var(&chans, "chans", "comma separated list of irc channels to join")
+	flag.Var(&channels, "channels", "comma separated list of irc channels to join")
 }
 
 const usage = `
@@ -28,31 +28,31 @@ You can query location using "hny" trigger for example "hny New York"
 
 CMD Options:
 [mandatory]
--chans			comma separated list of irc channels to join eg. "#test, #test2"
--botnick		nick for the bot
--email			referrer email for Nominatim
+-channels	comma separated list of irc channels to join eg. "#test, #test2"
+-nick		nick for the bot
+-email		referrer email for Nominatim
 
 [optional]
--nickpass		nick password
--ircserver		irc server to use (default: irc.freenode.net:7000)
--trigger		trigger used for queries. (default: hny)
--usetls			use tls encryption for irc. (default: true)
--nominatim		Nominatim server to use (default: http://nominatim.openstreetmap.org)
--ircdebug		log irc traffic
+-password	nick password
+-server		irc server to use (default: chat.freenode.net:6697)
+-trigger	trigger used for queries. (default: hny)
+-ssl		use ssl encryption for irc. (default: true)
+-nominatim	Nominatim server to use (default: http://nominatim.openstreetmap.org)
+-debug		debug irc traffic
 
 `
 
 func main() {
 
 	//Flags
-	botnick := flag.String("botnick", "", "irc nick for the bot")
+	nick := flag.String("nick", "", "irc nick for the bot")
 	email := flag.String("email", "", "referrer email for Nominatim")
-	ircServer := flag.String("ircserver", "chat.freenode.net:6697", "irc server to use")
-	nickpass := flag.String("nickpass", "", "nick password")
+	server := flag.String("server", "chat.freenode.net:6697", "irc server to use")
+	password := flag.String("password", "", "nick password")
 	trigger := flag.String("trigger", "hny", "trigger for queries")
-	useTLS := flag.Bool("usetls", true, "use tls for irc")
+	ssl := flag.Bool("ssl", true, "use ssl for irc")
 	nominatim := flag.String("nominatim", "http://nominatim.openstreetmap.org", "nominatim server to use")
-	ircdebug := flag.Bool("ircdebug", false, "log irc traffic")
+	debug := flag.Bool("debug", false, "debug irc traffic")
 
 	green := color.New(color.FgGreen)
 	flag.Usage = func() {
@@ -64,31 +64,31 @@ func main() {
 	red := color.New(color.FgHiRed)
 
 	//Check mandatory inputs
-	if len(chans) == 0 {
+	if len(channels) == 0 {
 		red.Fprintln(os.Stderr, "error: no channels defined")
 		flag.Usage()
 		return
 	}
 	chanReg := regexp.MustCompile("^([#&][^\\x07\\x2C\\s]{0,200})$")
-	for _, ch := range chans {
+	for _, ch := range channels {
 		if !chanReg.MatchString(ch) {
 			red.Fprintf(os.Stderr, "error: invalid channel name: %s\n", ch)
 			flag.Usage()
 			return
 		}
 	}
-	if *botnick == "" {
+	if *nick == "" {
 		red.Fprintln(os.Stderr, "error: no nick defined")
 		flag.Usage()
 		return
 	}
-	if len(*botnick) > 16 {
+	if len(*nick) > 16 {
 		red.Fprintln(os.Stderr, "error: nick can't be longer than 16 characters")
 		flag.Usage()
 		return
 	}
 	botnickReg := regexp.MustCompile("^\\A[a-z_\\-\\[\\]\\^{}|`][a-z0-9_\\-\\[\\]\\^{}|`]{1,15}\\z$")
-	if !botnickReg.MatchString(*botnick) {
+	if !botnickReg.MatchString(*nick) {
 		red.Fprintln(os.Stderr, "error: invalid nickname")
 		flag.Usage()
 		return
@@ -104,13 +104,13 @@ func main() {
 		return
 	}
 	//Check optional inputs
-	if *ircServer == "" {
+	if *server == "" {
 		red.Fprintln(os.Stderr, "error: no irc server defined")
 		flag.Usage()
 		return
 	}
 	serverReg := regexp.MustCompile("^\\S+:\\d+$")
-	if !serverReg.MatchString(*ircServer) {
+	if !serverReg.MatchString(*server) {
 		red.Fprintln(os.Stderr, "error: invalid irc server address")
 		flag.Usage()
 		return
@@ -137,8 +137,8 @@ func main() {
 		return
 	}
 
-	bot := nyb.New(*botnick, chans, *nickpass, *trigger, *ircServer, *useTLS, *email, *nominatim)
-	if *ircdebug {
+	bot := nyb.New(*nick, channels, *password, *trigger, *server, *ssl, *email, *nominatim)
+	if *debug {
 		bot.LogLvl(log.LvlDebug)
 	} else {
 		bot.LogLvl(log.LvlInfo)
