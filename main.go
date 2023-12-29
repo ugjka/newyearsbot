cbc9e0c8ace2138ab2561c9e150ffaafb760e9ee
RhinosF1 <github@rhinosf1.com>
2023-12-29 21:17:33 +0000

    Update main.go

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 	//Flags
 	nick := flag.String("nick", "NewYearBot", "irc nick")
 	email := flag.String("email", "", "nominatim email")
-	server := flag.String("server", "irc.libera.chat:6697", "irc server")
+	server := flag.String("server", "irc.snoonet.org:6697", "irc server")
 	password := flag.String("password", "", "irc password")
 	prefix := flag.String("prefix", "!", "command prefix")
 	nossl := flag.Bool("nossl", false, "disable ssl for irc")
