76751e5b7897d215731a1346be64c6e276a2b9fd
Uģis Ģērmanis <ugis@germanis.lv>
2025-01-24 08:30:52 +0200

    extract only the csv file we need

--- nyb/update_tz_csv.sh
+++ nyb/update_tz_csv.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 curl https://timezonedb.com/files/TimeZoneDB.csv.zip > time_zone.csv.zip
-unzip -o time_zone.csv.zip
-rm database.sql
-rm country.csv
+unzip -o time_zone.csv.zip time_zone.csv
 rm time_zone.csv.zip
-rm README
